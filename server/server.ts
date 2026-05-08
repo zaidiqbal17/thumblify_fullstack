e5253833cb1fbe4cecde681e29049697f3d7dba2
ZAID IQBAL <zaidiqbal171717@gmail.com>
2026-05-08 16:57:44 +0530

    Adeed Cors Origin

--- server/server.ts
+++ server/server.ts
@@ -20,7 +20,7 @@ await connectDB()
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://thumblify-client-five.vercel.app'],
     credentials: true
 }))
 
